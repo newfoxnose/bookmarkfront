060e8d24d3b48225143f91f223aa905f3611a759
newfoxnose <bel81nan@hotmail.com>
2023-02-16 10:39:43 +0800

    20230216

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,74 @@
 <script setup>
+import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
+
+ 
+<script>
+ 
+export default {
+  methods: {
+    // 点击按钮，获取图书列表数据
+    async btnGetBooks () {
+      const { data: res } = await this.$http.get('/index/')
+      console.log(res)
+    }
+  }
+ 
+}
+</script>
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
     </div>
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <button @click="btnGetBooks">获取图书列表</button>
+  <RouterView />
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
+  max-height: 100vh;
 }
 
 .logo {
   display: block;
   margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
 }
 
 @media (min-width: 1024px) {
@@ -42,6 +86,15 @@ header {
     display: flex;
     place-items: flex-start;
     flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
   }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+ 
+/* 导入axios进行全局配置 */
+import axios from 'axios'
+
+
+const app = createApp(App)
+
+ 
+/* 配置请求的根路径 */
+axios.defaults.baseURL='http://bookmark.com'
+ 
+/* 将 axios 挂载到全局，今后，每个组件中，
+都可以直接通过this.$http 代替 axios 发起 Ajax 请求 */
+app.config.globalProperties.$http=axios
+
+
+
+app.use(router)
+
+app.mount('#app')
