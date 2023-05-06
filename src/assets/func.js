3829ba33bef68614f6ec7755b002f7ad883ed2a1
newfoxnose <bel81nan@hotmail.com>
2023-05-06 16:59:32 +0800

    qiniu

--- src/assets/func.js
+++ src/assets/func.js
@@ -5,4 +5,18 @@ export default {
   https() {
     showDrawer
   },
+  formatterSizeUnit(size) {
+    if (size) {
+        var result = parseFloat(size);
+        if (result < 1024) {
+            return result.toFixed(0) + "B";
+        } else if (result < 1024 * 1024) {
+            return (result / 1024).toFixed(1) + "KB";
+        } else if (result < 1024 * 1024 * 1024) {
+            return (result / (1024 * 1024)).toFixed(1) + "MB";
+        } else {
+            return (result / (1024 * 1024 * 1024)).toFixed(1) + "GB";
+        }
+    }
+}
 }
