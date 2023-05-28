c07f51dc2b3287f9992de41708fa59df01aa404f
newfoxnose <bel81nan@hotmail.com>
2023-05-29 07:52:36 +0800

    首页随机显示公开书签

--- src/main.js
+++ src/main.js
@@ -16,8 +16,8 @@ import func from '@/assets/func'
 const app = createApp(App);
 
 /* 配置请求的根路径，远程后端 */
-//axios.defaults.baseURL = "https://bm.gm.ws";
-axios.defaults.baseURL = "http://bookmark.com";
+axios.defaults.baseURL = "https://bm.gm.ws";
+//axios.defaults.baseURL = "http://bookmark.com";
 
 /* 将 axios 挂载到全局，今后，每个组件中，
 都可以直接通过this.$http 代替 axios 发起 Ajax 请求 */
