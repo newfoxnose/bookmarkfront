c38326937c7222471f44c61d6e084ea2e655b8b1
newfoxnose <bel81nan@hotmail.com>
2023-05-31 10:50:13 +0800

    20230531

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,45 +1,14 @@
-<script setup>
-import bookmarkitem from './bookmarkitem.vue'
-import { defineComponent, ref } from 'vue';
-import {
-  CloseOutlined,
-} from '@ant-design/icons-vue';
 
-</script>
- 
 <script>
 export default {
   data() {
     return {
       items: {},
-      question: '',
-      search: '',
-      editable:'no'
-    }
-  },
-  watch: {
-    // 每当 question 改变时，这个函数就会执行
-    question(newQuestion, oldQuestion) {
-      this.search = newQuestion
-    }
-  },
-  methods: {
-    clearQuestion() {
-      this.question = ''
-      this.search = ''
     }
   },
   async mounted() {
-    console.log("mounted")
     const { data: res } = await this.$http.get('/ajax/index_ajax')
     this.items = res.data
-    console.log(this.items)
-    console.log("ajax requested")
-  },
-  defineComponent() {
-    components: {
-      CloseOutlined
-    }
   },
 }
 
@@ -53,25 +22,7 @@ export default {
     </a>
   </div>
 
-  <div>
-    <bookmarkitem v-for="bookmarkitem in items.root_bookmarks" :id="bookmarkitem.id" :folder_id="bookmarkitem.folder_id"
-      :url="bookmarkitem.url" :title="bookmarkitem.title" :short_title="bookmarkitem.short_title"
-      :is_private="bookmarkitem.is_private" :icon="bookmarkitem.icon_display" :search="search" :editable="editable"></bookmarkitem>
-
-  </div>
   <div style="margin-bottom:20px;">
-  </div>
-
-  <div class="search-div">
-    <div class="search">
-
-      <a-input v-model:value="question">
-        <template #addonAfter>
-          <close-outlined @click="clearQuestion" />
-        </template>
-      </a-input>
-
-    </div>
   </div>
 </template>
 
