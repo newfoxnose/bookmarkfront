667460ef4f3e07c8f5983e6179aeb52518870ab8
newfoxnose <bel81nan@hotmail.com>
2024-09-12 11:32:55 +0800

    20240912

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="loadingbar" v-show="loadingdone == false">
+  <div class="loadingbar" v-show="loadingdone == false">
     <a-progress
       type="circle"
       :percent="defaultPercent"
@@ -11,36 +11,15 @@
       }"
     />
   </div>
-    <h3 style="margin-top:15px;">写笔记</h3>
-  <a-button type="primary" @click="save" :loading="iconLoading">保存</a-button>
-  <a-form :model="formState">
-    <a-form-item label="标题" name="title" :rules="[{ required: true, message: '标题不能为空' }]">
-      <a-input v-model:value="formState.title" />
-    </a-form-item>
-    <p>
-      <a-select style="width: 100%" v-model:value="formState.folder_id" v-if="folder_list">
-        <a-select-option v-for="item in folder_list" :value="item.value" :lv="item.lv"> {{ item.name }}</a-select-option>
-      </a-select>
-    </p>
-    <p>
-      <a-checkbox v-model:checked="formState.is_recommend">置顶</a-checkbox>
-      <a-checkbox v-model:checked="formState.is_private">不公开</a-checkbox>
-    </p>
-  </a-form>
-  <vue-ueditor-wrap v-model="valueHtml" :config="editorConfig" editor-id="editor-demo-01"></vue-ueditor-wrap>
-
-  <div>
-    <a-modal v-model:visible="visible" title="新建笔记成功" @ok="handleOk" ok-text="确认" cancel-text="取消">
-      <p>跳转到笔记列表页请点击“确定”</p>
-      <p>再写一篇请点击“取消”</p>
-    </a-modal>
-  </div>
+  <p style="margin-top:15px;">
+    <a-button type="primary" @click="showDrawer('新建笔记', 0)"><form-outlined />新建笔记</a-button
+      ></p>
   <div>
     <a-image-preview-group>
       <div v-for="item in fileitems" style="margin-bottom: 5px">
         <span class="ext">{{ item.id }}</span>
-        <a :href="'/editblog/' + item.id" style="margin-left: 5px">
-          {{  item.title=='' ? '无标题' : item.title }}
+        <a @click="showDrawer('笔记', item.id)" style="margin-left: 5px">
+          {{ item.title == "" ? "无标题" : item.title }}
         </a>
         <eye-invisible-two-tone
           v-if="item.is_private == '1'"
@@ -51,7 +30,9 @@
           style="margin-left: 3px"
         />
         <span style="margin-left: 20px">( {{ item.createtime }})</span>
-        <a style="margin-left: 20px" @click="deletepost(item.id,currentpage)">删除</a>
+        <a style="margin-left: 20px" @click="showconfirmdelete(item.id, currentpage)"
+          >删除</a
+        >
         <br /><a-image
           v-for="img in item.img_arr"
           :key="img.label"
@@ -61,9 +42,76 @@
       </div>
     </a-image-preview-group>
   </div>
-  <div style="clear:both;">
-  <a-pagination v-model:current="currentpage" v-model:pageSize="pagesize" :total="total" @change="handlepagechange" show-less-items />
+  <div style="clear: both">
+    <a-pagination
+      v-model:current="currentpage"
+      v-model:pageSize="pagesize"
+      :total="total"
+      @change="handlepagechange"
+      show-less-items
+    />
   </div>
+
+  <a-drawer
+    :width="500"
+    :height="600"
+    :title="updatedDrawerTitle"
+    placement="bottom"
+    :visible="visible"
+    @close="onClose"
+  >
+    <template #extra>
+      <a-button type="primary" @click="save()" :loading="iconLoading"
+        >保存</a-button>
+      &nbsp;
+      <a-button style="margin-right: 8px" @click="onClose">取消</a-button>
+    </template>
+
+    <a-form :model="formState">
+      <a-form-item
+        label="标题"
+        name="title"
+        :rules="[{ required: true, message: '标题不能为空' }]"
+      >
+        <a-input v-model:value="formState.title" />
+      </a-form-item>
+
+      <a-row>
+        <a-col :span="12">
+          <p>
+            <a-select
+              style="width: 100%"
+              v-model:value="formState.folder_id"
+              v-if="folder_list"
+            >
+              <a-select-option
+                v-for="item in folder_list"
+                :value="item.value"
+                :lv="item.lv"
+              >
+                {{ item.name }}</a-select-option
+              >
+            </a-select>
+          </p></a-col
+        >
+        <a-col :span="12">
+          <p style="padding-left: 20px">
+            <a-checkbox v-model:checked="formState.is_recommend"
+              >置顶</a-checkbox
+            >
+            <a-checkbox v-model:checked="formState.is_private"
+              >不公开</a-checkbox
+            >
+          </p></a-col
+        >
+      </a-row>
+    </a-form>
+    <vue-ueditor-wrap
+      v-model="valueHtml"
+      :config="editorConfig"
+      editor-id="editor-demo-01"
+    ></vue-ueditor-wrap>
+  </a-drawer>
 </template>
 <style scoped>
 .ext {
@@ -89,35 +137,36 @@
 }
 </style>
 <script>
-import { message } from "ant-design-vue";
+import { message, Modal } from "ant-design-vue";
 
 import {
   InboxOutlined,
   EyeInvisibleTwoTone,
   LikeTwoTone,
+  StarOutlined,FormOutlined,
 } from "@ant-design/icons-vue";
 import { onMounted, getCurrentInstance, defineComponent, ref } from "vue";
 import * as qiniu from "qiniu-js";
 import { Base64 } from "js-base64";
-import { useRouter } from 'vue-router'
+import { useRouter } from "vue-router";
 export default {
   components: {
     InboxOutlined,
     EyeInvisibleTwoTone,
     LikeTwoTone,
+    StarOutlined,FormOutlined,
   },
   setup() {
-
-    const formState = ref([])
-    formState.value.title = '';
+    const formState = ref([]);
+    formState.value.title = "";
     formState.value.is_private = false;
     formState.value.is_recommend = false;
-    const folder_list = ref([])
-    const router = useRouter()
+    const folder_list = ref([]);
+    const router = useRouter();
     const iconLoading = ref(false);
 
     // 内容 HTML
-    const valueHtml = ref('<p>hello</p>')
+    const valueHtml = ref("<p>写点什么呢？</p>");
 
     const defaultPercent = ref(10);
     const loadingdone = ref(false);
@@ -130,17 +179,75 @@ export default {
     const file_key = ref("");
     const fileitems = ref([]);
 
+    const updatedDrawerTitle = ref(String);
+    const visible = ref(false);
+    const blog_id = ref(0);
+    const showDrawer = (drawerTitle, id) => {
+      visible.value = true;
+      updatedDrawerTitle.value = drawerTitle;
+      if (id != 0) {
+        blog_id.value = id;
+        let params = new URLSearchParams(); //post内容必须这样传递，不然后台获取不到
+        params.append("token", $cookies.get("token"));
+        params.append("timestamp", new Date().getTime());
+        params.append("blog_id", blog_id.value);
+        proxy.$http.post("/ajax/get_blog_ajax/", params).then((res) => {
+          //console.log(res.data.data.blog)
+          valueHtml.value = res.data.data.blog.content;
+          defaultPercent.value = 100;
+          loadingdone.value = true;
+          formState.value.title = res.data.data.blog.title;
+          formState.value.folder_id = res.data.data.blog.folder_id;
+          if (res.data.data.blog.is_private == 1) {
+            formState.value.is_private = true;
+          } else {
+            formState.value.is_private = false;
+          }
+          if (res.data.data.blog.is_recommend == 1) {
+            formState.value.is_recommend = true;
+          } else {
+            formState.value.is_recommend = false;
+          }
+        });
+      } else {
+        formState.value.title = "";
+        formState.value.folder_id = -1;
+        formState.value.is_private = false;
+        formState.value.is_recommend = false;
+        valueHtml.value = "<p>写点什么呢？</p>";
+      }
+    };
+
+    const showconfirmdelete = (editId, currentpage) => {
+      Modal.confirm({
+        title: "确认删除该项目吗？",
+        content:
+          "点击OK删除且无法找回, 点击cancel取消",
+        onOk() {
+          deletepost(editId, currentpage);
+        },
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        onCancel() {},
+      });
+    };
+    const onClose = () => {
+      iconLoading.value = false;
+      visible.value = false;
+      blog_id.value = 0;
+    };
+
     onMounted(() => {
-      let params = new URLSearchParams();    //post内容必须这样传递，不然后台获取不到
-      params.append("token", $cookies.get('token'));
-          params.append("timestamp",new Date().getTime());
-      proxy.$http.post('/ajax/get_folder_ajax/', params).then(res => {
-        if (res.data.code=='401'){      //不在登陆状态
-      window.location.href ="/";
-    }
-        folder_list.value = res.data.data.data
-        formState.value.folder_id = res.data.data.data[0].value
-      })
+      let params = new URLSearchParams(); //post内容必须这样传递，不然后台获取不到
+      params.append("token", $cookies.get("token"));
+      params.append("timestamp", new Date().getTime());
+      proxy.$http.post("/ajax/get_folder_ajax/", params).then((res) => {
+        if (res.data.code == "401") {
+          //不在登陆状态
+          window.location.href = "/";
+        }
+        folder_list.value = res.data.data.data;
+        formState.value.folder_id = res.data.data.data[0].value;
+      });
       handlepagechange(1);
     });
     const handlepagechange = (values) => {
@@ -168,7 +275,7 @@ export default {
           loadingdone.value = true;
         });
     };
-    const deletepost = (id,page) => {
+    const deletepost = (id, page) => {
       let params = new URLSearchParams(); //post内容必须这样传递，不然后台获取不到
       params.append("token", $cookies.get("token"));
       params.append("timestamp", new Date().getTime());
@@ -186,56 +293,56 @@ export default {
     const save = () => {
       console.log(router.currentRoute.value.params);
       iconLoading.value = true;
-      if (proxy.$func.getVarType(formState.value.title) == "undefined" || formState.value.title == '') {
+      if (
+        proxy.$func.getVarType(formState.value.title) == "undefined" ||
+        formState.value.title == ""
+      ) {
         message.info("标题不能为空");
         iconLoading.value = false;
-      }
-      else {
-        let params = new URLSearchParams();    //post内容必须这样传递，不然后台获取不到
-        params.append("token", $cookies.get('token'));
-        params.append("timestamp",new Date().getTime());
+      } else {
+        let params = new URLSearchParams(); //post内容必须这样传递，不然后台获取不到
+        params.append("token", $cookies.get("token"));
+        params.append("timestamp", new Date().getTime());
         params.append("content", valueHtml.value);
         params.append("title", formState.value.title);
         params.append("folder_id", formState.value.folder_id);
+        if (blog_id.value!=0){
+          params.append("post_id", blog_id.value);
+        }
         if (formState.value.is_private == true) {
           params.append("is_private", 1);
-        }
-        else {
+        } else {
           params.append("is_private", 0);
         }
         if (formState.value.is_recommend == true) {
           params.append("is_recommend", 1);
-        }
-        else {
+        } else {
           params.append("is_recommend", 0);
         }
-        proxy.$http.post('/ajax/save_blog_ajax/', params).then(res => {
-          //console.log(res.data.msg);
-          if (res.data.msg == "新建成功") {    //这条提示如果改的话要和后端一起改
-            visible.value = true;
-            //message.info("已保存，即将跳转到列表页面",10);
+        proxy.$http
+          .post("/ajax/save_blog_ajax/", params)
+          .then((res) => {
+            //console.log(res.data.msg);
+            if (res.data.msg == "新建成功") {
+              //这条提示如果改的话要和后端一起改
+              message.info("新建笔记成功");     //看不到效果
+              iconLoading.value = false;
+              handlepagechange(1);
+            } else {
+              message.info(res.data.msg);
+              iconLoading.value = false;
+              handlepagechange(1);
+            }
+            onClose();
+          })
+          .catch((error) => {
+            message.info("无法正常保存");
             iconLoading.value = false;
-            //window.location.href = "/blog";
-          }
-          else{
-            message.info(res.data.msg);
-            iconLoading.value = false;
-          }
-        }).catch(error => {
-          message.info("无法正常保存");
-          iconLoading.value = false;
-          console.log(error);
-        });
+            console.log(error);
+          });
       }
-    }
-    const visible = ref(false);
-    const handleOk = e => {
-      visible.value = false;
-      window.location.href = "/blog";
     };
     return {
-      visible,
-      handleOk,
       formState,
       folder_list,
       valueHtml,
@@ -251,6 +358,12 @@ export default {
       currentpage,
       pagesize,
       total,
+      visible,
+      showDrawer,
+      onClose,
+      updatedDrawerTitle,
+      blog_id,
+      showconfirmdelete,
     };
   },
   created() {
@@ -262,12 +375,17 @@ export default {
         ['bold', 'italic', 'underline', 'fontborder', 'strikethrough', 'superscript', 'subscript', 'removeformat', 'formatmatch', 'autotypeset', 'blockquote', 'pasteplain', '|', 'forecolor', 'backcolor', 'insertorderedlist', 'insertunorderedlist', 'selectall', 'cleardoc','simpleupload']
       ],
       */
-      UEDITOR_HOME_URL: '/UEditor/', // 访问 UEditor 静态资源的根路径，可参考常见问题1
-      lang: 'zh-cn',
+      UEDITOR_HOME_URL: "/UEditor/", // 访问 UEditor 静态资源的根路径，可参考常见问题1
+      lang: "zh-cn",
       // 初始容器高度
-      initialFrameHeight: 360,
-      serverUrl: this.$remoteDomain+'/ueditor/controller.php?token='+$cookies.get('token')+'&timestamp='+(new Date().getTime()), // 服务端接口
+      initialFrameHeight: 300,
+      serverUrl:
+        this.$remoteDomain +
+        "/ueditor/controller.php?token=" +
+        $cookies.get("token") +
+        "&timestamp=" +
+        new Date().getTime(), // 服务端接口
     };
-  }
+  },
 };
 </script>
