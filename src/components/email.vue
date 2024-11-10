91b6863c7a07a4a48fdecc9df3651bff17af0d5e
newfoxnose <bel81nan@hotmail.com>
2024-11-10 08:30:41 +0800

    vuetify

--- src/components/email.vue
+++ src/components/email.vue
@@ -1,29 +1,35 @@
 <script>
-import { message } from 'ant-design-vue';
-export default ({
-  setup(){
-    $cookies.set('selectedkey','13',"720h") 
-    $cookies.set('openkey','sub1',"720h") 
+import { message } from "ant-design-vue";
+export default {
+  setup() {
+    $cookies.set("selectedkey", "13", "720h");
+    $cookies.set("openkey", "sub1", "720h");
   },
   mounted() {
-
-    let params = new URLSearchParams();    //post内容必须这样传递，不然后台获取不到
-    params.append("token", $cookies.get('token'));
-          params.append("timestamp",new Date().getTime());
-    this.$http.post('/ajax/send_email_ajax/', params).then(res => {
-      console.log(res.data.data);
-      if (res.data.code=='401'){      //不在登陆状态
-      window.location.href ="/";
-    }
-      message.info(res.data.msg);
-    }).catch(error => {
-      // obj.error ? obj.error(error) : null;
-      console.log(error);
-    });
-  }
-});
+    let params = new URLSearchParams(); //post内容必须这样传递，不然后台获取不到
+    params.append("token", $cookies.get("token"));
+    params.append("timestamp", new Date().getTime());
+    this.$http
+      .post("/ajax/send_email_ajax/", params)
+      .then((res) => {
+        console.log(res.data.data);
+        if (res.data.code == "401") {
+          //不在登陆状态
+          window.location.href = "/";
+        }
+        message.info(res.data.msg);
+      })
+      .catch((error) => {
+        // obj.error ? obj.error(error) : null;
+        console.log(error);
+      });
+  },
+};
 </script>
 <template>
-  <a-result title="发送至邮箱" sub-title="请在提示成功5分钟后到邮箱查看，也有可能在垃圾邮件里">
+  <a-result
+    title="发送至邮箱"
+    sub-title="请在提示成功5分钟后到邮箱查看，也有可能在垃圾邮件里"
+  >
   </a-result>
 </template>
